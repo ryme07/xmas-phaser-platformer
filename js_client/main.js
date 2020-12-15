490c0a5a57fbbf6799f3b81d0a02b1a819380fb8
ryme07 <remydelrieu@hotmail.fr>
2020-12-15 18:58:48 +0100

    feat: world spritesheet & player

--- js_client/main.js
+++ js_client/main.js
@@ -1,5 +1,6 @@
-const config = {
+var config = {
   type: Phaser.AUTO,
+  backgroundColor: "#ccccff",
   width: 800,
   height: 600,
   scene: {
@@ -16,7 +17,38 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+var cameraControls;
 
-function preload() {}
-function create() {}
-function update(time, delta) {}
+function preload() {
+  this.load.image("tiles", "tilesheet.png");
+  this.load.tilemapTiledJSON("map", "platformer.json");
+  this.load.atlas("player", "player.png", "playerAtlas.json");
+}
+function create() {
+  this.tilemap = this.make.tilemap({ key: "map" });
+  this.tileset = this.tilemap.addTilesetImage("tilesheet", "tiles");
+
+  var player = this.add.sprite(200, 200, "player", "Idle1");
+
+  this.downLayer = this.tilemap.createStaticLayer("bot", this.tileset, 0, 0);
+  this.worldLayer = this.tilemap.createStaticLayer("world", this.tileset, 0, 0);
+  this.topLayer = this.tilemap.createStaticLayer("top", this.tileset, 0, 0);
+
+  var cursors = this.input.keyboard.createCursorKeys();
+
+  var controlCameraConfig = {
+    camera: this.cameras.main,
+    left: cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5,
+  };
+
+  cameraControls = new Phaser.Cameras.Controls.FixedKeyControl(
+    controlCameraConfig
+  );
+}
+function update(time, delta) {
+  cameraControls.update(delta);
+}
